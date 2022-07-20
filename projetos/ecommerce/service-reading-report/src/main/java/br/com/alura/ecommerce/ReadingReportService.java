3ca623c433142cb379ae9737bb71bc07ba5b32a7
josepaulosjc <jose_paulo_sjc@hotmail.com>
2022-07-20 20:46:37 -0300

    Extraindo camada de serviço no Consumer e implementando threads.

--- projetos/ecommerce/service-reading-report/src/main/java/br/com/alura/ecommerce/ReadingReportService.java
+++ projetos/ecommerce/service-reading-report/src/main/java/br/com/alura/ecommerce/ReadingReportService.java
@@ -1,6 +1,8 @@
 package br.com.alura.ecommerce;
 
+import br.com.alura.ecommerce.consumer.ConsumerService;
 import br.com.alura.ecommerce.consumer.KafkaService;
+import br.com.alura.ecommerce.consumer.ServiceRunner;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.io.File;
@@ -9,21 +11,15 @@ import java.nio.file.Path;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class ReadingReportService {
+public class ReadingReportService implements ConsumerService<User> { //Consome Users
 
     private static final Path SOURCE = new File("src/main/resources/report.txt").toPath();
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var reportService = new ReadingReportService();
-        try(var service = new KafkaService<>(ReadingReportService.class.getSimpleName(), //Processo de desserialização de Order
-                "ECOMMERCE_USER_GENERATE_READING_REPORT",
-                reportService::parse,
-                Map.of())) { // Nesse service não temos propriedades extras então passamos um mapa vazio.
-            service.run();
-        }
+    public static void main(String[] args) {
+        new ServiceRunner(ReadingReportService::new).start(5); //permite a executar o serviço com multithread
     }
 
-    private void parse(ConsumerRecord<String, Message<User>> record) throws IOException {
+    public void parse(ConsumerRecord<String, Message<User>> record) throws IOException {
         System.out.println("---------------------------------------------");
         var message = record.value();
         var user = message.getPayload();
@@ -34,5 +30,15 @@ public class ReadingReportService {
         IO.append(target, "Created for " + user.getUuid());
 
         System.out.println("File created: " + target.getAbsolutePath());
+    }
+
+    @Override
+    public String getConsumerGroup() {
+        return ReadingReportService.class.getSimpleName();
+    }
+
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_USER_GENERATE_READING_REPORT";
     }
 }
