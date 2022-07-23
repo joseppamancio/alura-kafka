6a972f77f522e581e0821070def58d54d1dba876
josepaulosjc <jose_paulo_sjc@hotmail.com>
2022-07-23 09:43:49 -0300

    Consumindo Mensagens com AUTO_OFFSET_REST_CONFIG.

--- projetos/ecommerce/common-kafka/src/main/java/br/com/alura/ecommerce/consumer/ConsumerService.java
+++ projetos/ecommerce/common-kafka/src/main/java/br/com/alura/ecommerce/consumer/ConsumerService.java
@@ -4,9 +4,10 @@ import br.com.alura.ecommerce.Message;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public interface ConsumerService<T> { // metodos padrão de um serviço de consumo
-    void parse(ConsumerRecord< String, Message<T>> record) throws IOException;
+    void parse(ConsumerRecord< String, Message<T>> record) throws IOException, ExecutionException, InterruptedException;
     String getConsumerGroup();
     String getTopic();
     }
--- projetos/ecommerce/service-email-new-order/src/main/java/br/com/alura/ecommerce/EmailNewOrderService.java
+++ projetos/ecommerce/service-email-new-order/src/main/java/br/com/alura/ecommerce/EmailNewOrderService.java
@@ -1,27 +1,20 @@
 package br.com.alura.ecommerce;
 
-import br.com.alura.ecommerce.consumer.KafkaService;
+import br.com.alura.ecommerce.consumer.ConsumerService;
+import br.com.alura.ecommerce.consumer.ServiceRunner;
 import br.com.alura.ecommerce.dispatcher.KafkaDispatcher;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.math.BigDecimal;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class EmailNewOrderService {
+public class EmailNewOrderService implements ConsumerService<Order> {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var emailService = new EmailNewOrderService();
-        try(var service = new KafkaService<>(EmailNewOrderService.class.getSimpleName(), //Processo de desserialização de Order
-                "ECOMMERCE_NEW_ORDER",
-                emailService::parse,
-                Map.of())) { // Nesse service não temos propriedades extras então passamos um mapa vazio.
-            service.run();
-        }
+    public static void main(String[] args) {
+        new ServiceRunner(EmailNewOrderService::new).start(1); //permite a executar o serviço com multithread
     }
 
     private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
-    private void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
         System.out.println("---------------------------------------------");
         System.out.println("Processando new order, preparing email");
         var message = record.value();
@@ -31,5 +24,14 @@ public class EmailNewOrderService {
         var order = message.getPayload();
         var id = message.getId().continueWith(EmailNewOrderService.class.getSimpleName());
         emailDispatcher.send("ECOMMERCE_SEND_EMAIL", order.getEmail(), id, emailCode);
+    }
+    @Override
+    public String getConsumerGroup() {
+        return EmailNewOrderService.class.getSimpleName();
+    }
+    // Escuta Topico - ECOMMERCE_NEW_ORDER
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_NEW_ORDER";
     }
 }
