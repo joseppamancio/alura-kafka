155ccdf6a277e3e1d6abcbcc71109fde27c1fcb6
José Paulo P Amâncio <54041843+joseppamancio@users.noreply.github.com>
2023-08-02 16:49:41 -0300

    Update start_zookeeper.sh

--- kafka-arquivos/start_zookeeper.sh
+++ kafka-arquivos/start_zookeeper.sh
@@ -1,1 +1,1 @@
-sudo /home/pi/kafka_2.13-3.2.0/bin/zookeeper-server-start.sh /home/pi/kafka_2.13-3.2.0/config/zookeeper.properties
+sudo /home/pi/kafka_2.12-3.2.0/bin/zookeeper-server-start.sh /home/pi/kafka_2.12-3.2.0/config/zookeeper.properties
