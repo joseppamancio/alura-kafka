2ca13c8c259653d08f4647aa9a0a921c7346e90b
José Paulo P Amâncio <54041843+joseppamancio@users.noreply.github.com>
2023-08-02 16:49:20 -0300

    Update start_kafka_server3.sh

--- kafka-arquivos/start_kafka_server3.sh
+++ kafka-arquivos/start_kafka_server3.sh
@@ -1,1 +1,1 @@
-/home/pi/kafka_2.13-3.2.0/bin/kafka-server-start.sh /home/pi/kafka_2.13-3.2.0/config/server3.properties
+/home/pi/kafka_2.12-3.2.0/bin/kafka-server-start.sh /home/pi/kafka_2.12-3.2.0/config/server3.properties
